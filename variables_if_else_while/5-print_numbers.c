20504f3b4fecf9c257be19b021ea4471a39e17d9
LachyBM <13072@holbertonstudents.com>
2026-02-11 04:25:14 +0000

    5 added

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a-z lower case
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+
+	return (0);
+}
