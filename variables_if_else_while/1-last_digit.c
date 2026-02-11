db7dd4d026feb9c136531f109850d38d3ebe058a
LachyBM <13072@holbertonstudents.com>
2026-02-11 03:43:15 +0000

    real fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, last);
 	}
-	else if(last == 0)
+	else if (last == 0)
 	{
 		printf("Last digit of %i is %i and is 0\n", n, last);
 	}
