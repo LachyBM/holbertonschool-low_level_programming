f4bf2d1dda6c6b35aed8a7b577198f223c95ff92
LachyBM <13072@holbertonstudents.com>
2026-02-11 04:14:11 +0000

    4 added

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - a-z lower case
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch == 'q' || ch == 'e')
+	{
+	}
+	else
+	{
+	putchar(ch);
+	}
+
+	}
+	putchar ('\n');
+
+return (0);
+}
