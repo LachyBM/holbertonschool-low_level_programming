20c3f6eaac6838d7ed1098ef4df4c36898d359d1
LachyBM <13072@holbertonstudents.com>
2026-02-12 08:13:45 +0000

    added 0/main/readme

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a-z lower case
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
