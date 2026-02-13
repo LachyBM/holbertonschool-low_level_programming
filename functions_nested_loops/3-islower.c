16a71cb94ca3affa569208b5d0e75348b06f876a
LachyBM <13072@holbertonstudents.com>
2026-02-13 22:59:20 +0000

    3/main

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - prints is lowercase
+ * @c: interger
+ * Return: 0.
+ */
+
+int _islower(int c)
+{
+	if (c <= 'a' && c >= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
