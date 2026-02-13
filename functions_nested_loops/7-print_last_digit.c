b334b1fb43ccf327999f7e508c60be50c1ad5541
LachyBM <13072@holbertonstudents.com>
2026-02-13 23:33:35 +0000

    7 adeed main updated

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit- prints is lowercase
+ * @n: interger
+ * Return: 0.
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last = -last;
+	}
+	_putchar(last + '0');
+	return (last);
+}
