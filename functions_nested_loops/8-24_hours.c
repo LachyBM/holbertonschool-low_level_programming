3326e69818e9aa719b026965b105097421247325
LachyBM <13072@holbertonstudents.com>
2026-02-13 23:51:56 +0000

    added 8 updated main

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *jack_bauer- print abs
+ *
+ * Return: 0.
+ */
+
+void jack_bauer(void)
+{
+	int hour = 0;
+
+	while (hour < 24)
+	{
+
+		int minute = 0;
+
+		while (minte < 60)
+		{
+			_putchar('hour / 10' + 0);
+			_putchar('hour % 10' + 0);
+			_putchar(':');
+			_putchar('minute / 10' + 0);
+			_putchar('minute % 10' + 0);
+			_putchar('\n');
+			minute++;
+		}
+		hour++;
+	}
+}
