58ebe2a46255eb4e4ef2163caefba51a366068a4
LachyBM <13072@holbertonstudents.com>
2026-02-14 00:20:57 +0000

    added 9 stuff

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *jack_bauer- print abs
+ *
+ * Return: 0.
+ */
+
+void times_table(void)
+{
+	int n1 = 0;
+
+	while (n1 <= 9)
+	{
+		int n2 = 0;
+
+		while (n2 <= 9)
+		{
+		int multi = n1 * n2;
+		
+		if (multi >= 10)
+		{
+			_putchar((multi / 10) + '0');
+			_putchar((multi % 10) + '0');
+		}
+		else
+			_putchar( multi + '0');
+		if (n2 < 9)
+		{
+                	_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		n2++;
+		}
+	_putchar('\n');
+	n1++;
+	}
+}
