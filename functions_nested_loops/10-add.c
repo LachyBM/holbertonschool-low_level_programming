5c8cca8bd810e4847dfe6bd4a8719b78d692fe94
LachyBM <13072@holbertonstudents.com>
2026-02-14 12:01:32 +0000

    10 added

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add- print addition of 2 numbers
+ * @n: interger 1
+ * @x: interger 2
+ * Return: sum
+ */
+
+int add(int n, int x)
+{
+	int sum = n + x;
+	return (sum);
+}
