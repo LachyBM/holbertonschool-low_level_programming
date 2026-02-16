f2a49956786d2d11e931513220174e2312623685
LachyBM <13072@holbertonstudents.com>
2026-02-16 05:40:23 +0000

    added 7, update main

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal- prints diagonal line
+ * @n: interger
+ * Return: 0.
+ */
+
+void print_diagonal(int n)
+{
+	int x = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	while (x < n)
+	{
+		int diag = 0;
+		while (diag < x)
+		{
+			_putchar(' ');
+			diag++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		x++;
+	}
+
+	}
+}
