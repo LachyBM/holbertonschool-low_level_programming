6e2db55eaca53199514b314ada8ceff2f12b87eb
LachyBM <13072@holbertonstudents.com>
2026-02-16 05:16:53 +0000

    6 added/ updated main

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_line- prints multli
+ * @n: interger
+ * Return: 0.
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	while (x < n)
+	{
+		_putchar('_');
+		x++;
+	}
+		_putchar('\n');
+	}
+}
