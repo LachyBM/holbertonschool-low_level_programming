6fedf65df9403eaa9966df0bd95f8d1d60076083
LachyBM <13072@holbertonstudents.com>
2026-02-16 05:50:21 +0000

    added 8/updated main

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_square- prints multli
+ * @size: interger
+ * Return: 0.
+ */
+
+void print_square(int size)
+{
+
+	int y = 0;
+
+	while (size > y)
+	{
+
+	int x = 0;
+
+	while (size > x)
+	{
+		_putchar('#');
+		x++;
+	}
+	_putchar('\n');
+	y++;
+	}
+}
