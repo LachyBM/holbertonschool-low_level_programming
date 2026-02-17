0e2dcda7ec9453b023283d82799fc1a1c6a5c8c7
LachyBM <13072@holbertonstudents.com>
2026-02-17 02:08:27 +0000

    added starting 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string- reverses string
+ * @s:  char
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (length >= 0)
+	{
+		char letter = s[length];
+		char 
+
+		printf("%c",letter);
+		length--;
+		i++;
+	}
+	printf("\n");
+}
