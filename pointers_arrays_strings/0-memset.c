777821bdc1aed05a5992e83ac8b90f326c1218af
LachyBM <13072@holbertonstudents.com>
2026-02-23 23:29:13 +0000

    0 and main added

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset- fills first n bytes of memory area to by s with sontant byte b
+ * @s-char
+ * @b -char
+ * @n - unsigned int
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (n > i)
+		{
+		s[i] = b;
+		i++;	
+		}
+	return (s);
+}
