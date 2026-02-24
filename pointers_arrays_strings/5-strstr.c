92cccc67a9c4bc1733720db022ae40225bb2bfdc
LachyBM <13072@holbertonstudents.com>
2026-02-24 01:16:19 +0000

    added

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr- returns number of bytes in s which consit only of bytes from accept
+ * @s:char
+ * @accept:char
+ * Return:value
+ */
+
+char *_strstr(char *s, char *accept)
+{
+	int i = 0;
+	int length = 0;
+
+	while (accept[length] != '\0')
+	{
+		length++;
+	}
+
+	while (s[i] != '\0')
+	{
+		int x = 0;
+
+		if (s[i] == accept[x])
+		{
+			while (accept[x] != '\0')
+			{
+				if (s[i] == accept[x])
+				{
+					i++;
+					x++;
+				}
+				else
+				{
+					break;
+				}
+
+				if (x == length)
+				{
+					return (&s[i] - x);
+				}
+			}
+
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (NULL);
+}
