8d2494b584cec76923e47686b7e3457d6bd169d2
LachyBM <13072@holbertonstudents.com>
2026-02-25 01:55:34 +0000

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion- length of the string
+ * @s: char
+ * Return: the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
