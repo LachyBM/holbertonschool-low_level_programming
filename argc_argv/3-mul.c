ec36e74dac722183aa2340698eed2daf3aaf2185
LachyBM <13072@holbertonstudents.com>
2026-02-28 00:54:22 +0000

    added mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- print out number
+ * @argc: char
+ * @argv: int
+ * Return: value
+ */
+
+int main(int argc, char **argv)
+{
+	int mul;
+	int x;
+	int y;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (0);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	mul = x * y;
+	printf("%d\n", mul);
+
+	return (0);
+}
