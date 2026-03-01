9f32eb2fdeafb20baebd35fc5510ca85ed2363a9
LachyBM <13072@holbertonstudents.com>
2026-03-01 01:00:03 +0000

    added prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,6 +2,22 @@
 #include "main.h"
 
 /**
+ * helper- helps check for prime
+ * @n: int
+ * @check: int
+ * Return: primer
+ */
+
+int helper(int n, int check)
+{
+	if (check * check > n)
+		return (1);
+	if (n % check == 0)
+		return (0);
+	return (helper(n, check + 1));
+}
+
+/**
  * is_prime_number- check if number is prime
  * @n: int  number to check
  * Return: if prime or not
@@ -9,5 +25,7 @@
 
 int is_prime_number(int n)
 {
-
+	if (n <= 1)
+		return (0);
+	return (helper(n, 2));
 }
