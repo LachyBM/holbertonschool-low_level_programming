3652a63ae48c3d8de739c2fd0bfbecf7f5dfb098
LachyBM <13072@holbertonstudents.com>
2026-03-02 02:18:30 +0000

    added 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *create_array- creates an array
+ * @str: char
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int size = 0;
+	char *copy;
+
+	if (str == NULL)
+        {
+                return (NULL);
+        }
+	
+	while (str[size] != '\0')
+	{
+		size++;
+	}	
+	
+	copy = malloc(size * sizeof(char));
+
+	while (i < size)
+	{
+		copy[i] = str[i];
+		i++;
+	}
+
+	return (copy);
+}
