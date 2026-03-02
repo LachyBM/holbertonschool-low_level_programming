93e7080a7f960a71701499d64e58c6e53c247180
LachyBM <13072@holbertonstudents.com>
2026-03-02 02:39:07 +0000

    updated

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,10 +17,11 @@ char *str_concat(char *s1, char *s2)
 	int total = 0;
 	char *combo;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[size] != '\0')
 	{
 		size++;
@@ -33,9 +34,8 @@ char *str_concat(char *s1, char *s2)
 
 	combo = malloc((total + 1)  * sizeof(char));
 	if (combo == NULL)
-	{
 		return (NULL);
-	}
+
 	while (i < size)
 	{
 		combo[i] = s1[i];
