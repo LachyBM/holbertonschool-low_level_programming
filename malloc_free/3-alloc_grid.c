104f604a19afd0aee46f220a882719f50e547762
LachyBM <13072@holbertonstudents.com>
2026-03-02 04:22:39 +0000

    added?

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid- grid
+ * @width: int width of grid
+ * @height: int highet of grid
+ * Return: grid with locations
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+	int i, j = 0;
+	int **grid = malloc(height * sizeof(int *));
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	while (i < height)
+	{
+		grid[i] = malloc(width * sizeof(int));
+
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (grid);
+}
