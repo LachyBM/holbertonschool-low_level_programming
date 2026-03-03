520c45905ea5c0839d2a7374dd2e7326f0389c4d
LachyBM <13072@holbertonstudents.com>
2026-03-03 06:05:24 +0000

    added

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *array_range- array range
+ * @min: int lowest number
+ * @max: int highet number
+ * Return: pointer location
+ */
+
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int *p;
+	int count = 0;
+
+	while ((min + count - 1) < max)
+	{
+		count++;
+	}
+
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * count + 1);
+	if (p == NULL)
+		return (NULL);
+
+	while (i < count)
+	{
+		p[i] = min + i;
+		i++;
+	}
+	return (p);
+}
