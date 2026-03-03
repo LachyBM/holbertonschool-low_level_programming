cbb8c2a51e5df2933f1ccdc390297a9f4573effc
LachyBM <13072@holbertonstudents.com>
2026-03-03 06:20:53 +0000

    dog added and readme/dir

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dogs owner
+ *
+ * Description: dog struct to know dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
