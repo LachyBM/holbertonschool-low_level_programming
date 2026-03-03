b7b04a12ca71ab6f9cb1eef88e53082750729083
LachyBM <13072@holbertonstudents.com>
2026-03-03 06:35:52 +0000

    added

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog- int the dog
+ * @d: dog strust
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
