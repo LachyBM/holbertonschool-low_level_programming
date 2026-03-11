9919004085f6495112441ce392eba99b9d8c39a1
LachyBM <13072@holbertonstudents.com>
2026-03-11 13:57:36 +0000

    comp

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -3,14 +3,24 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator-
- * @array:
- * @size:
- * @action:
+ * array_iterator- function print array
+ * @array: array
+ * @size: size of array
+ * @action: action
  * Return: NOTHING
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t arr = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	while (arr  < size)
+	{
+		action(array[arr]);
+		arr++;
+	}
 
 }
