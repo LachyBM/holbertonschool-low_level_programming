3c3107dc78b2d3971e51ed4d0627d508983b3779
LachyBM <13072@holbertonstudents.com>
2026-03-11 11:24:57 +0000

    start

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator-
+ * @array:
+ * @size:
+ * @action:
+ * Return: NOTHING
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
