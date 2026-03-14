77a39ef829aeda260a0622bc067df72b930d701a
LachyBM <13072@holbertonstudents.com>
2026-03-14 14:31:43 +0000

    3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all- prints numbers with separator
+ * @format: the format
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *s;
+	int i = 0;
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		s = NULL;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				s = "";
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				s = "";
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				s = "";
+				break;
+			case 's':
+				printf("%s", va_arg(ap, char *));
+				s = "";
+				break;
+		}
+		if (s != NULL && format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
