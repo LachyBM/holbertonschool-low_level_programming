3a20d83d2cee5d837a70e90cb3ca046b2c049684
LachyBM <13072@holbertonstudents.com>
2026-03-23 01:00:24 +0000

    shells of 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node- prints a list
+ * @head: value
+ * @str: value
+ * Return: address
+ */
+
+list_t add_node(list_t **head, const char *str)
+{
+
+}
