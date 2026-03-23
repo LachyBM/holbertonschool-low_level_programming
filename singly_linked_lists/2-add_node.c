a719bf2de2a60165af34eab31ab8486d00077191
LachyBM <13072@holbertonstudents.com>
2026-03-23 23:26:55 +0000

    added 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,7 +10,13 @@
  * Return: address
  */
 
-list_t add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node = malloc(sizeof(list_t));
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 
+	return *head;
 }
