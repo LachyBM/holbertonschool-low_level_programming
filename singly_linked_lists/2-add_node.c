ae0741178c18a795ce8525935602205a5fe33d41
LachyBM <13072@holbertonstudents.com>
2026-03-23 23:35:44 +0000

    fixed 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,11 +12,21 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
+	list_t *new_node;
+	char *dup = strdup(str);
+	int length = 0;
+
+	while (dup[length])
+		length++;
+
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(dup);
+	new_node->len = length;
 	new_node->next = *head;
 	*head = new_node;
 
-	return *head;
+	return (new_node);
 }
