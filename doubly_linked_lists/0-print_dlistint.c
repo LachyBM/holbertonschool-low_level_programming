281bd2b71aef9e09abed96d0cf82251551625f53
LachyBM <13072@holbertonstudents.com>
2026-03-24 00:54:37 +0000

    fixed 0 to allow both directions

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -13,11 +13,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t len = 0;
 
+	while (h->next != NULL)
+		h = h->next;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		len++;
-		h = h->next;
+		h = h->prev;
 	}
 	return (len);
 }
