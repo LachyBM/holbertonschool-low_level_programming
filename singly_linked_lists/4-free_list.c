b7446fcc5b399209cc5e514ba6354d9d67bb827a
LachyBM <13072@holbertonstudents.com>
2026-03-24 00:03:22 +0000

    added 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list- frees list
+ * @head: value
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
