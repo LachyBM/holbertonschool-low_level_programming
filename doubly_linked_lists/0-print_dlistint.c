e7f7e282cf94d83ccf553becce287be33c3dbff9
LachyBM <13072@holbertonstudents.com>
2026-03-24 00:58:39 +0000

    I BEG PLZ

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -13,13 +13,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t len = 0;
 
-	while (h->next != NULL)
-		h = h->next;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		len++;
-		h = h->prev;
+		h = h->next;
 	}
 	return (len);
 }
