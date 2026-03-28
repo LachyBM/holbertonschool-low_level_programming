b7f9ed92ecc2debcdddeb1d99c42d1ef33eccdff
LachyBM <13072@holbertonstudents.com>
2026-03-28 01:28:13 +0000

    test3

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,15 +26,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	
+
 	while (head != NULL)
 	{
 		if (i == idx - 1)
 		{
+			if (head->next == NULL)
+				return (add_dnodeint_end(h, n));
 			new_node->n = n;
 			new_node->next = head->next;
 			new_node->prev = head;
-			head->next->prev = new_node;
 			head->next = new_node;
 
 			return (new_node);
