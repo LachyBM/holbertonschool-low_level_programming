c3b27000cd67da530e73775e3bb1016b2325f353
LachyBM <13072@holbertonstudents.com>
2026-04-01 09:14:57 +0000

    6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_delete-delt
+ * @ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *tmp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
